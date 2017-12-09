4ec1ab9e1a9e7051b147f8e7cf6c82ecbb04a62c
warriorsiri <616988782@qq.com>
2017-12-10 01:12:42 +0800

    第一个测试项目

---
+++ demo/src/main/java/com/warrior/demo/Test.java
@@ -0,0 +1,23 @@
+package com.warrior.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * Warrior
+ * 2017/12/10
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println();
+
+        List<Integer> ls = new ArrayList<Integer>();
+
+        Integer bb = ls.get(0);
+        System.out.println(bb);
+        System.out.println(bb);
+
+    }
+}
---
+++ demo/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<body>
+<h2>Hello World!</h2>
+你好！！！反对广泛大使馆犯得上
+</body>
+</html>
